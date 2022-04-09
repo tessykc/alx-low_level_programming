41313c6c27817abc225a3268f243ca6a4c20fbf2
tessykc <tessykc@gmail.com>
2022-04-08 21:41:02 -0700

    My commit for this work

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,30 +18,29 @@
  */
 int main(void)
 {
-        int c;
-        int d = 0;
+	int c;
+	int d = 0;
 
-        while (d < 10)
-        {
-                c = 0;
-                while (c < 10)
-                {
-                        if (d != c && d < c)
-                        {
-                                putchar('0' + d);
-                                putchar('0' + c);
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
 
-                                if (c + d != 17)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-
-                        c++;
-                }
-                d++;
-        }
-        putchar('\n');
-        return (0);
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
 }
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,37 +18,36 @@
  */
 int main(void)
 {
-        int c;
-        int d;
-        int e = 0;
+	int c;
+	int d;
+	int e = 0;
 
-        while (e < 10)
-        {
-                d = 0;
-                while (d < 10)
-                {
-                        c = 0;
-                        while (c < 10)
-                        {
-                                if (c != d && d != e && e < d && d < c)
-                                {
-                                        putchar('0' + e);
-                                        putchar('0' + d);
-                                        putchar('0' + c);
+	while (e < 10)
+	{
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				if (c != d && d != e && e < d && d < c)
+				{
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
 
-                                        if (c + d + e != 9 + 8 + 7)
-                                        {
-                                                putchar(',');
-                                                putchar(' ');
-                                        }
-                                }
-
-                                c++;
-                        }
-                        d++;
-                }
-                e++;
-        }
-        putchar('\n');
-        return (0);
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				c++;
+			}
+			d++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
 }

7a2197bcabc2da0c1cb95d6b500c62507dff94da
tessykc <tessykc@gmail.com>
2022-04-24 08:57:15 -0700

    Commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,9 +13,9 @@ int _strcmp(char *s1, char *s2)
 
 	while (f == 0)
 	{
-		if ((s1 + i) == '\0') && (*(s2 + i) == '\0'))
+		if ((s1 + i) == '\0') && (*(s2 + i) == '\0')
 			break;
-		f = *9s1 + i) - *(s2 + i);
+		f = *(s1 + i) - *(s2 + i);
 		i++;
 	}
 	return (f);

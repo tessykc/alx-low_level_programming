e108a3b9c831ac6c32edb780bc9bcdb070eeb63a
tessykc <tessykc@gmail.com>
2022-05-03 07:26:35 -0700

    Mandatory task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,25 @@
 #include "main.h"
 
 /**
- * *_strcat - concatenates two strings
- * @dest: pointer destination
- * @src: pointer source
- * Return: void
+ * _strcat - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ * Return: address of dest
  */
-char *_strcat(char *dest, char *src)
-{	
-	int a = -1, i;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-	do {
-		a++;
-		dest[i] = src[a];
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = j = 0;
+	while (*(dest + i))
+	{
 		i++;
 	}
-	while (src[a] != '\0');
-	return (dest);
+	while ((*(dest + i) = *(src + j))
+	{
+		i++;
+		j++;
+	}
+		return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,27 @@
 #include "main.h"
 
 /**
- * *_strncat - concatenates n bytes of two strings
- * @dest: pointer destination
- * @src: pointer source
- * @n: number of bytes
- * Return: void
+ * _strncat - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ * @n: largest number of bytes to append
+ * Return: address of dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len, i;
+	int i, j;
 
-	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
-		;
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
-	/*should end with a end of string char*/
-	dest[dest_len + i] = '\0';
-
+	i = j = 0;
+	while (*(dest + i))
+		i++;
+	while (j < n && *(src + j))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	if (j < n)
+		*(dest + i) = *(src + j);
 	return (dest);
 }

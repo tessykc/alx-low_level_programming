08d7eacbb5afe916a369bd5d0a46f9bbd13b0c10
tessykc <tessykc@gmail.com>
2022-06-01 08:08:54 -0700

    correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,24 +5,24 @@
  * @b: pointer to a string containing a binary number
  * Return: unsigned int with decimal value, or 0 if error
  */
-unsigned int binary_to_uint(const char *b) 
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int num;
 
-	num = 0;
+	num = 0;
 	if (!b)
-		return (0);
-	for (i = 0; b[i] != '\0'; i++)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 	}
-	for (i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		num <<= 1;
-		if (b[i] == '1')
-			num += 1;
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
 	}
-	return (num);
+	return (num);
 }

21fbd6fad6584d40c7eae0c4e27ea99c214e6018
tessykc <tessykc@gmail.com>
2022-07-13 23:51:43 -0700

    operation

---
+++ 0x18-dynamic_libraries/5-sub.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * sub - subtracts two integers
+ * @a: int to be subtracted to b
+ * @b: int to be subtracted to a
+ * Return: the result of the operation
+ */
+int sub(int a, int b)
+{
+	int c;
+
+	c = a - b;
+	return (c);
+}

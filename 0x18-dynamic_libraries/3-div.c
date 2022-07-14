9b31957aae419591e75f66ebe9f4043fc3822a8e
tessykc <tessykc@gmail.com>
2022-07-13 23:31:53 -0700

    operation

---
+++ 0x18-dynamic_libraries/3-div.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * div - divides two integers
+ * @a: int to be divided to b
+ * @b: int to be divided to a
+ * Return: the result of the operation
+ */
+int div(int a, int b)
+{
+	int c;
+
+	while
+	{
+		b != 0
+		c = a / b;
+	}
+	return (c)
+}
---
+++ 0x18-dynamic_libraries/4-mod.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mod - checks the remainder of  two integers
+ * @a: int to be divided to b
+ * @b: int to be divided to a
+ * Return: the result of the operation
+ */
+int mod(int a, int b)
+{
+	int c;
+
+	if b != 0
+		c = a % b;
+	return (c)
+}

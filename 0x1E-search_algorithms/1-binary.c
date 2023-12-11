363f8a38d98a2d9e486eaec6fa1d4d6d50133e97
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2023-12-11 11:27:48 +0100

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,4 +1,10 @@
 #include "main.h"
 /**
-*
+* Where array is a pointer to the first element of the array to search in
+* size is the number of elements in array
+* And value is the value to search for
+* Your function must return the index where value is located
+* You can assume that array will be sorted in ascending order
+* You can assume that value won’t appear more than once in array
+* if array is NULL, your function must return -1
 */

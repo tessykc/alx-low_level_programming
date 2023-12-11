25bfc584dbd2c5253140a2327aac3a9f06bb9bb4
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2023-12-11 11:19:30 +0100

    Create 0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+* Where array is a pointer to the first element of the array to search in
+* size is the number of elements in array
+* And value is the value to search for
+* Your function must return the first index where value is located
+* if array is NULL, your function must return -1
+*/

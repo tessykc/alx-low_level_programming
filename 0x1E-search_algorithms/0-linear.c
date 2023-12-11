d56acb542f68179cd958df015496f8b78b8dced4
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2023-12-11 11:24:08 +0100

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,5 +7,12 @@
 * if array is NULL, your function must return -1
 */
 int linear_search(int *array, size_t size, int value){
-  
+  if (array == NULL) {
+    return -1; // Return -1 if the array is NULL
+}
+for (size_t i = 0; i < size; i++) {
+  if (array[i] == value) {
+    return i; // Return the index if the value is found
+        }
+}
 }

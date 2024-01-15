1e121a1e3fdfff5a8093409aaba4ff8832960d57
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2024-01-15 10:27:12 +0100

    search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,8 @@ int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 /**
  * struct listint_s - singly linked list
  *
@@ -20,7 +22,6 @@ typedef struct listint_s
     size_t index;
     struct listint_s *next;
 } listint_t;
-listint_t *jump_list(listint_t *list, size_t size, int value);
 /**
  * struct skiplist_s - Singly linked list with an express lane
  *
@@ -38,5 +39,5 @@ typedef struct skiplist_s
     struct skiplist_s *next;
     struct skiplist_s *express;
 } skiplist_t;
-skiplist_t *linear_skip(skiplist_t *list, int value);
+
 #endif
